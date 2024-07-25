c70b6a28264b7141d8af06fb9ec6c7899f9e5282
PrabhakaranRavi <prabhakaranravi93@gmail.com>
2024-07-25 13:21:25 +0530

    Folder Structure and First Script

---
+++ RestAPI/src/test/java/ValidateStatusCode/statusValidate.java
@@ -0,0 +1,16 @@
+package ValidateStatusCode;
+
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+
+public class statusValidate {
+    @Test
+    public void statusCode(){
+                given().
+                when().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                assertThat().
+                statusCode(200);
+    }
+}
