a082ebe47e797aeb33e0a578f1d4457825a3043f
PrabhakaranRavi <prabhakaranravi93@gmail.com>
2024-07-27 10:02:35 +0530

    Update statusValidate.java

--- RestAPI/src/test/java/ValidateStatusCode/statusValidate.java
+++ RestAPI/src/test/java/ValidateStatusCode/statusValidate.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static junit.framework.Assert.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -94,5 +95,86 @@ public class statusValidate {
         //Use Hamcrest to check that response body contains specific items in specific order
         List<String> expectedEmails = Arrays.asList("Eliseo@gardner.biz","Jayne_Kuhic@sydney.com","Nikita@garfield.biz","Lew@alysha.tv","Hayden@althea.biz");
         assertThat(response.jsonPath().getList("email"),contains(expectedEmails.toArray(new String[0])));
+    }
+
+    @Test
+    public void validateListByIs(){
+        //Set Base URI for the API
+        RestAssured.baseURI = "https://reqres.in/api/";
+
+        // Send GET request and store the response in a variable
+        Response response = given().
+                when().
+                get("users?page=2");
+
+
+        //Assert that response contains 6 users
+        response.then().body("data", hasSize(6));
+
+        //Assert that the first user in the list has the correct values - IS Matcher
+        response.then().body("data[0].id",is(7));
+        response.then().body("data[0].email",is("michael.lawson@reqres.in"));
+        response.then().body("data[0].first_name",is("Michael"));
+        response.then().body("data[0].last_name",is("Lawson"));
+        response.then().body("data[0].avatar",is("https://reqres.in/img/faces/7-image.jpg"));
+
+        response.then().body("data[1].id",equalTo(8));
+        response.then().body("data[1].email",equalTo("lindsay.ferguson@reqres.in"));
+        response.then().body("data[1].first_name",equalTo("Lindsay"));
+        response.then().body("data[1].last_name",equalTo("Ferguson"));
+        response.then().body("data[1].avatar",equalTo("https://reqres.in/img/faces/8-image.jpg"));
+
+    }
+
+    @Test
+    public void queryParam(){
+        //Set Base URI for the API
+        RestAssured.baseURI = "https://reqres.in/api/";
+
+        // Send GET request and store the response in a variable
+        Response response = given().
+                queryParam("page",2).
+                when().
+                get("users");
+
+        //Saving response code
+        int actualStatusCode =response.statusCode(); //RestAssured
+        assertThat(actualStatusCode,equalTo(200));
+        assertEquals(actualStatusCode,200);
+    }
+
+    @Test
+    public void multiQueryParam(){
+        //Set Base URI for the API
+        RestAssured.baseURI = "https://reqres.in/api/";
+
+        // Send GET request and store the response in a variable
+        Response response = given().
+                queryParam("page",2).
+                queryParam("per_page",3).
+                when().
+                get("users");
+
+        //Saving response code
+        int actualStatusCode =response.statusCode(); //RestAssured
+        assertThat(actualStatusCode,equalTo(200));
+        assertEquals(actualStatusCode,200);
+    }
+
+    @Test(description = "Using path param, Getting the response of Circuits")
+    public void pathParam(){
+        //Set Base URI for the API
+        RestAssured.baseURI = "https://ergast.com/api/";
+        String raceSeasonValue = "2017";
+        // Send GET request and store the response in a variable
+        Response response = given().
+                pathParam("raceSeason",raceSeasonValue).
+                when().
+                get("f1/{raceSeason}/circuits.json");
+
+        //Saving response code
+        int actualStatusCode =response.statusCode(); //RestAssured
+        assertThat(actualStatusCode,equalTo(200));
+        System.out.println(response.body().asString());
     }
 }
